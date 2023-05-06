bdd01593af533da67ddbe64a25ed3288c5c80667
devalexnow <a.nowak.dev@gmail.com>
2023-05-06 15:27:05 +0200

    decimal

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function sendNumberValue(number) {
 const displayValue = calculatorDisplay.textContent;
 calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
+
+function addDecimal() {
+    // If no decimal, add one
+    if(!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
  
 // Add Event Listeners for numbers, operators, decimal buttons
 inputBtns.forEach((inputBtn) => {
@@ -15,6 +22,15 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => addDecimal());
     }
 });
+
+// Reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
